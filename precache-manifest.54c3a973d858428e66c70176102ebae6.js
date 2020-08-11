7ea32f5e3b5f5b947b6c9a9a9efc2fb86993755c
papyrinIS <papyrin@sumirea.ru>
2020-08-11 18:04:35 +0300

    Updates

---
+++ precache-manifest.54c3a973d858428e66c70176102ebae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf5e71a1d4f18ae946ea6b5cb4a53fd",
+    "url": "/weather-dashboard/index.html"
+  },
+  {
+    "revision": "5752f72ffc26cdcd1836",
+    "url": "/weather-dashboard/static/css/main.b62a0865.chunk.css"
+  },
+  {
+    "revision": "d3de0cb765ec3591f601",
+    "url": "/weather-dashboard/static/js/2.c56b233d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-dashboard/static/js/2.c56b233d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5752f72ffc26cdcd1836",
+    "url": "/weather-dashboard/static/js/main.c3dc849e.chunk.js"
+  },
+  {
+    "revision": "31db064ffbeeb79930dd",
+    "url": "/weather-dashboard/static/js/runtime-main.a6a7c5e2.js"
+  }
+]);
